b36640926a311771db2952712623deeffef8c4b7
Cleber Faria <webfaria@gmail.com>
2021-05-26 15:34:21 -0300

    Tests Service With Mockito

--- src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -10,20 +10,33 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
 
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private long existingId;
 	private long nonExistingId;
-	
+	private long countTotalProduct;
+
 	@BeforeEach
-	void setup() throws Exception{
+	void setup() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
+		countTotalProduct = 25L;
+	}
+
+	@Test
+	public void saveShoudPersistWithAutoincrementWhenIdIsNull() {
+		Product product = Factory.createdProduct();
+		product.setId(null);
+		product = repository.save(product);
+
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProduct + 1, product.getId());
 	}
 
 	@Test
@@ -33,7 +46,7 @@ public class ProductRepositoryTests {
 		Optional<Product> result = repository.findById(existingId);
 		Assertions.assertFalse(result.isPresent());
 	}
-	
+
 	@Test
 	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
 
@@ -41,6 +54,18 @@ public class ProductRepositoryTests {
 			repository.deleteById(nonExistingId);
 		});
 
+	}
+	
+	@Test
+	public void findByIdShouldReturnNonEmptyOptiontWhenIdExists() {
+		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertTrue(result.isPresent());
+	}
+	
+	@Test
+	public void findByIdShouldReturnEmptyOptiontWhenIdDoesNotExists() {
+		Optional<Product> result = repository.findById(nonExistingId);
+		Assertions.assertTrue(result.isEmpty());
 	}
 
 }
